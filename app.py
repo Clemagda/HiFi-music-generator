57eaa14f563580b0397a92596359ca0baae144ba
Clemagda <125815203+Clemagda@users.noreply.github.com>
2024-06-04 12:20:51 +0200

    Added audio generation and dowloading function.

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 # Installation des packages et bilbiothèques nécessaires
-
 from audiocraft.models import MusicGen
 import streamlit as st
 import os
@@ -15,6 +14,72 @@ def load_model():
     return model
 
 
+def generate_music_tensor(description, duration: int):
+    """Generate aduio tensors and tokens 
+
+    Args:
+        description (_type_): string
+        duration (int): _description_
+
+    Returns:
+        torch tensors: first dimension of tensor generated
+    """
+    print(f"Description : {description}")
+    print(f"Duration: {duration}")
+    model = load_model()
+
+    model.set_generation_params(
+        use_sampling=True,
+        top_k=250,
+        duration=duration
+    )
+
+    outputs = model.generate(descriptions=description,
+                             progress=True,
+                             return_tokens=True)
+
+    return outputs[0]
+
+
+def save_audio(samples: torch.tensor):
+    """Save generated audio to .wav format
+
+    Args:
+        samples (torch.tensor): _description_
+    """
+    sample_rate = 32000,
+    save_path = "audio_outputs/"
+
+    assert samples.dim() == 2 or samples.dims() == 3
+
+    samples = samples.detach().cpu()
+
+    if samples.dim() == 2:
+        samples = samples[None, ...]
+
+        for idx, audio in enumerate(samples):
+            audio_path = os.path.join(save_path, f"audio_{idx}.wav")
+            torchaudio.save(audio_path, audio, sample_rate)
+
+
+def get_binary_file_downloader_html(bin_file, file_label='File'):
+    """Download generated audio file.
+
+    Args:
+        bin_file (_type_): _description_
+        file_label (str, optional): _description_. Defaults to 'File'.
+
+    Returns:
+        _type_: _description_
+    """
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+
+    bin_str = base64.b64encode(data).decode()
+    href = f'<a href="data:application/octet-stream; base64,{bin_str}" download="{os.path.basename(bin_file)}">Download {file_label}<a>'
+    return href
+
+
 st.set_page_config(
     page_icon=":notes:",  # icones disponibles sur la doc streamlit
     page_title="Music Gen"
@@ -27,7 +92,7 @@ def main():
         st.write(
             "This is a music generation application based on Music Gen model. Based on your natural language description, it'll generate your desired music")
     text_area = st.text_area("Enter your music description here...")
-    time_slider = st.slider("Select time duration (in seconds)", 2, 5, 20)
+    time_slider = st.slider("Select time duration (in seconds)", 2, 20, 5)
 
     if text_area and time_slider:
         st.json(
@@ -37,7 +102,20 @@ def main():
             }
         )
         st.subheader("Generating...")
-    pass
+
+        music_tensors = generate_music_tensor(text_area, time_slider)
+
+        print(f"Music tensors : {music_tensors}")
+
+        save_music_file = save_audio(music_tensors)
+        audio_file_path = "audio_outputs/audio_0.wav"
+        # TODO: modifier audio_file_path pour une indexatio dynamique.Actullement, aucun audio n'est gardé en mémoire.
+        audio_file = open(audio_file_path, 'rb')
+        audio_bytes = audio_file.read()
+
+        st.audio(audio_bytes)
+        st.markdown(get_binary_file_downloader_html(
+            audio_file_path, 'Audio'), unsafe_allow_html=True)
 
 
 # TODO: afficher interface streamlit
